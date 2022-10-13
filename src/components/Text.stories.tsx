7555939331ef174a20e1d7889910780c656ced1f
gabriel <gabriel.pereira@ceuma.br>
2022-10-13 18:45:35 -0300

    first commit

---
+++ src/components/Text.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import { Text, TextProps } from "./Text";
+
+export default {
+  title: 'Components/Text',
+  component: Text,
+  args: {
+    children: 'Hello World'
+  }
+} as Meta<TextProps>
+
+export const Default: StoryObj<TextProps> = {}
+
+export const Small: StoryObj<TextProps> = {
+  args: {
+    size: 'sm'
+  }
+}
+
+export const Large: StoryObj<TextProps> = {
+
+  args: {
+    size: 'lg'
+  }
+}
